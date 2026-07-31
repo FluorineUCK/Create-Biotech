e66fcf7d65a3999e20a0179f2242307cd4f629e0
Nobodiiiii <mmu503690@gmail.com>
2026-07-31 16:38:15 +0800

    思索

--- src/main/java/com/nobodiiiii/createbiotech/mixin/NetherPortalBlockMixin.java
+++ src/main/java/com/nobodiiiii/createbiotech/mixin/NetherPortalBlockMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.BlockUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.NetherPortalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -38,6 +39,9 @@ public abstract class NetherPortalBlockMixin implements EntityBlock {
 	@Override
 	@Nullable
 	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		if (!state.is(Blocks.NETHER_PORTAL))
+			return null;
+
 		return new NetherPortalFluidBlockEntity(pos, state);
 	}
 
