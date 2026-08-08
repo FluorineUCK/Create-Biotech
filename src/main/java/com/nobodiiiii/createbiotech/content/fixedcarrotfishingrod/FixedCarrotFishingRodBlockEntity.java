d2785e68e19440f56172e8aa2435ab7863f999fc
rickwen1234 <rickwen132435@gmail.com>
2026-08-08 11:34:42 +0800

    fix: fixedfishingrod duplicate fix

--- src/main/java/com/nobodiiiii/createbiotech/content/fixedcarrotfishingrod/FixedCarrotFishingRodBlockEntity.java
+++ src/main/java/com/nobodiiiii/createbiotech/content/fixedcarrotfishingrod/FixedCarrotFishingRodBlockEntity.java
@@ -1,29 +1,28 @@
 package com.nobodiiiii.createbiotech.content.fixedcarrotfishingrod;
 
-import net.minecraft.core.HolderLookup;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import java.util.UUID;
+
+import javax.annotation.Nullable;
 
 import com.nobodiiiii.createbiotech.foundation.advancement.PlacedByPlayerAdvancementTracker;
 import com.nobodiiiii.createbiotech.registry.CBBlockEntityTypes;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.Clearable;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.neoforged.neoforge.items.ItemStackHandler;
 
-public class FixedCarrotFishingRodBlockEntity extends BlockEntity {
+public class FixedCarrotFishingRodBlockEntity extends BlockEntity implements Clearable {
 
 	@Nullable
 	private UUID advancementOwner;
@@ -110,4 +109,9 @@ public class FixedCarrotFishingRodBlockEntity extends BlockEntity {
 	public Packet<ClientGamePacketListener> getUpdatePacket() {
 		return ClientboundBlockEntityDataPacket.create(this);
 	}
+
+	@Override
+    public void clearContent() {
+        setBaitItem(ItemStack.EMPTY);
+    }
 }
