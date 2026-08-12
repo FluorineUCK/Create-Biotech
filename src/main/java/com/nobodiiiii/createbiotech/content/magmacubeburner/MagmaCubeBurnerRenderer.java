06b39aa83fb7eff6aaed74766db52959f1699dc3
Nobodiiiii <mmu503690@gmail.com>
2026-08-12 11:44:45 +0800

    燃烧室

--- src/main/java/com/nobodiiiii/createbiotech/content/magmacubeburner/MagmaCubeBurnerRenderer.java
+++ src/main/java/com/nobodiiiii/createbiotech/content/magmacubeburner/MagmaCubeBurnerRenderer.java
@@ -4,6 +4,8 @@ import javax.annotation.Nullable;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.nobodiiiii.createbiotech.foundation.render.EntityRenderHelper;
+import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
+import com.simibubi.create.content.processing.burner.BlazeBurnerBlock.HeatLevel;
 import com.simibubi.create.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
 
 import net.createmod.catnip.platform.NeoForgeCatnipServices;
@@ -11,6 +13,7 @@ import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.MagmaCube;
 import net.minecraft.world.level.Level;
@@ -19,6 +22,10 @@ import net.neoforged.neoforge.fluids.FluidStack;
 public class MagmaCubeBurnerRenderer extends SmartBlockEntityRenderer<MagmaCubeBurnerBlockEntity> {
 
 	private static final int MAGMA_CUBE_SIZE = 1;
+	private static final float MAGMA_CUBE_BASE_Y = 2f / 16f;
+	private static final float MAGMA_CUBE_JUMP_HEIGHT = 2f / 16f;
+	private static final float JUMP_ANIMATION_PERIOD = 40f;
+	private static final float JUMP_AIR_TIME = 16f;
 	private static final float FLUID_MIN_XZ = 3f / 16f + 1f / 512f;
 	private static final float FLUID_MAX_XZ = 13f / 16f - 1f / 512f;
 	private static final float FLUID_MIN_Y = 2f / 16f + 1f / 512f;
@@ -45,15 +52,54 @@ public class MagmaCubeBurnerRenderer extends SmartBlockEntityRenderer<MagmaCubeB
 		if (magmaCube == null)
 			return;
 		magmaCube.setSize(MAGMA_CUBE_SIZE, false);
+		updateJumpAnimation(magmaCube, blockEntity, partialTicks);
 
 		Direction facing = blockEntity.getBlockState().getValue(MagmaCubeBurnerBlock.FACING);
 		poseStack.pushPose();
-		poseStack.translate(.5, .125, .5);
+		poseStack.translate(.5, getMagmaCubeY(blockEntity, partialTicks), .5);
 		EntityRenderHelper.render(EntityRenderHelper.settings(magmaCube)
 			.packedLight(LightTexture.FULL_BRIGHT)
 			.partialTicks(partialTicks)
 			.face(facing), poseStack, buffer);
 		poseStack.popPose();
+	}
+
+	private static float getMagmaCubeY(MagmaCubeBurnerBlockEntity blockEntity, float partialTicks) {
+		HeatLevel heat = blockEntity.getBlockState().getValue(BlazeBurnerBlock.HEAT_LEVEL);
+		if (heat.isAtLeast(HeatLevel.KINDLED))
+			return MAGMA_CUBE_BASE_Y;
+
+		float cycleTick = (blockEntity.getLevel().getGameTime() + partialTicks) % JUMP_ANIMATION_PERIOD;
+		if (cycleTick >= JUMP_AIR_TIME)
+			return MAGMA_CUBE_BASE_Y;
+
+		float progress = cycleTick / JUMP_AIR_TIME;
+		float ballisticHeight = 4f * progress * (1f - progress);
+		return MAGMA_CUBE_BASE_Y + MAGMA_CUBE_JUMP_HEIGHT * ballisticHeight;
+	}
+
+	private static void updateJumpAnimation(MagmaCube magmaCube, MagmaCubeBurnerBlockEntity blockEntity,
+		float partialTicks) {
+		HeatLevel heat = blockEntity.getBlockState().getValue(BlazeBurnerBlock.HEAT_LEVEL);
+		if (!heat.isAtLeast(HeatLevel.KINDLED)) {
+			setSquish(magmaCube, 0);
+			return;
+		}
+
+		float time = blockEntity.getLevel().getGameTime() + partialTicks;
+		float phase = time % JUMP_ANIMATION_PERIOD / JUMP_ANIMATION_PERIOD;
+		float squish = 0;
+		if (phase < .2f)
+			squish = Mth.sin(phase / .2f * Mth.PI) * .5f;
+		else if (phase >= .8f)
+			squish = -Mth.sin((phase - .8f) / .2f * Mth.PI) * .25f;
+		setSquish(magmaCube, squish);
+	}
+
+	private static void setSquish(MagmaCube magmaCube, float squish) {
+		magmaCube.oSquish = squish;
+		magmaCube.squish = squish;
+		magmaCube.targetSquish = squish;
 	}
 
 	private static void renderLava(MagmaCubeBurnerBlockEntity blockEntity, PoseStack poseStack,
