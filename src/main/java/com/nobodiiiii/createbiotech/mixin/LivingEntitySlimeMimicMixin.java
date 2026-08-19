0dd434282b41593f5c11b0184fdb14e50cccee3d
Nobodiiiii <mmu503690@gmail.com>
2026-08-19 22:28:27 +0800

    黄油块

--- src/main/java/com/nobodiiiii/createbiotech/mixin/LivingEntitySlimeMimicMixin.java
+++ src/main/java/com/nobodiiiii/createbiotech/mixin/LivingEntitySlimeMimicMixin.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.nobodiiiii.createbiotech.content.buttercat.ButterRotationAccess;
 import com.nobodiiiii.createbiotech.content.slimemimic.SlimeMimicAccess;
 import com.nobodiiiii.createbiotech.content.slimemimic.SlimeMimicHandler;
 
@@ -18,27 +17,15 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.LivingEntity;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntitySlimeMimicMixin implements SlimeMimicAccess, ButterRotationAccess {
+public abstract class LivingEntitySlimeMimicMixin implements SlimeMimicAccess {
 
 	@Unique
 	private static final EntityDataAccessor<Boolean> CREATE_BIOTECH$SLIME_MIMIC = SynchedEntityData.defineId(
 		LivingEntity.class, EntityDataSerializers.BOOLEAN);
-	@Unique
-	private static final EntityDataAccessor<Integer> CREATE_BIOTECH$BUTTER_ROTATION_AMPLIFIER =
-		SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.INT);
-	@Unique
-	private static final EntityDataAccessor<Float> CREATE_BIOTECH$BUTTER_ROTATION_PHASE =
-		SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.FLOAT);
-	@Unique
-	private static final EntityDataAccessor<Long> CREATE_BIOTECH$BUTTER_ROTATION_PHASE_START_TICK =
-		SynchedEntityData.defineId(LivingEntity.class, EntityDataSerializers.LONG);
 
 	@Inject(method = "defineSynchedData", at = @At("TAIL"))
 	private void createBiotech$defineData(SynchedEntityData.Builder builder, CallbackInfo ci) {
 		builder.define(CREATE_BIOTECH$SLIME_MIMIC, false);
-		builder.define(CREATE_BIOTECH$BUTTER_ROTATION_AMPLIFIER, -1);
-		builder.define(CREATE_BIOTECH$BUTTER_ROTATION_PHASE, 0.0F);
-		builder.define(CREATE_BIOTECH$BUTTER_ROTATION_PHASE_START_TICK, -1L);
 	}
 
 	@Inject(method = "addAdditionalSaveData", at = @At("TAIL"))
@@ -61,41 +48,5 @@ public abstract class LivingEntitySlimeMimicMixin implements SlimeMimicAccess, B
 	@Override
 	public void createBiotech$setSlimeMimic(boolean slimeMimic) {
 		((LivingEntity) (Object) this).getEntityData().set(CREATE_BIOTECH$SLIME_MIMIC, slimeMimic);
-	}
-
-	@Override
-	public int createBiotech$getButterRotationAmplifier() {
-		return ((LivingEntity) (Object) this).getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_AMPLIFIER);
-	}
-
-	@Override
-	public void createBiotech$setButterRotationAmplifier(int amplifier) {
-		LivingEntity entity = (LivingEntity) (Object) this;
-		if (entity.getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_AMPLIFIER) != amplifier)
-			entity.getEntityData().set(CREATE_BIOTECH$BUTTER_ROTATION_AMPLIFIER, amplifier);
-	}
-
-	@Override
-	public float createBiotech$getButterRotationPhase() {
-		return ((LivingEntity) (Object) this).getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_PHASE);
-	}
-
-	@Override
-	public void createBiotech$setButterRotationPhase(float phase) {
-		LivingEntity entity = (LivingEntity) (Object) this;
-		if (Float.compare(entity.getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_PHASE), phase) != 0)
-			entity.getEntityData().set(CREATE_BIOTECH$BUTTER_ROTATION_PHASE, phase);
-	}
-
-	@Override
-	public long createBiotech$getButterRotationPhaseStartTick() {
-		return ((LivingEntity) (Object) this).getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_PHASE_START_TICK);
-	}
-
-	@Override
-	public void createBiotech$setButterRotationPhaseStartTick(long gameTime) {
-		LivingEntity entity = (LivingEntity) (Object) this;
-		if (entity.getEntityData().get(CREATE_BIOTECH$BUTTER_ROTATION_PHASE_START_TICK) != gameTime)
-			entity.getEntityData().set(CREATE_BIOTECH$BUTTER_ROTATION_PHASE_START_TICK, gameTime);
 	}
 }
